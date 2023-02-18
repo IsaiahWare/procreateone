c7190b7c94896a934832272bed832f2eeed88432
Isaiah Ware <isaiahware99@gmail.com>
2023-02-18 11:48:29 -0800

    Add files via upload

---
+++ time_cycles.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+
+struct Everything {
+    unsigned long long beginnings;
+    unsigned long long ends;
+};
+
+int time_cycle(int it, int d, struct Everything e, int max_it) {
+    e.beginnings += 1;
+    unsigned long long beginning = 0;
+    unsigned long long end = 1;
+
+    // Randomization is the language of time.
+    srand(time(NULL));
+    int choice = rand();
+
+    while (choice && it < max_it) {
+        e.ends += 1;
+        time_cycle(it+1,d+1,e, max_it);
+    }
+    return 0;
+}
+
+int anything() {
+    struct Everything e;
+    e.beginnings = 1;
+    e.ends = 0;
+    printf("%d ends for %d beginnings for a {%d / %d * 100}\% chance of going out of existence.", e.ends , e.beginnings, e.ends, e.beginnings);
+}
+
+int main() {
+    anything();
+    return 0;
+}
