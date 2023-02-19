02a3d1f8c8e2157253c846253b7930aa23f22b94
Isaiah Ware <iware@brex.com>
2023-02-19 09:01:07 -0800

    up date

--- nlgc.py
+++ nlgc.py
@@ -1,7 +1,6 @@
 from random import randint
 from sys import argv
 
-
 class Everything:
     def __init__(self, ends, beginnings):
         self.beginnings = beginnings
@@ -9,7 +8,6 @@ class Everything:
 
     def set(self):
         return (self.beginnings, self.ends)
-
 
 class Anything:
     def __init__(self, them):
@@ -19,51 +17,59 @@ class Anything:
         it = self.time_cycle(0, Everything(0, 1))
         self.them += [it]
         return self.them
-
+    
     def time_cycle(self, dimension, everything):
         everything.beginnings += 1
         choice = randint(0, 1)
+
         if choice:
             self.time_cycle(dimension+1, everything)
             everything.ends += 1
+
         return everything.set()
 
 class All:
-    def be(limit=0):
+    def be(Limit=0):
         DNA = []
-        while limit:
+
+        while Limit:
             DNA += [Anything([]).exist()]
-            limit -= 1
+            Limit -= 1
+
         return DNA
-    def void(limit):
-        return All.be(limit)
+    
+    def void(Limit):
+        return All.be(Limit)
 
 class Printer:
-    def run(name, limit, generations, connect):
+    def run(name, Limit, Generations, connect):
         with open(name, 'a' if connect else 'w') as f:
-            print(f'nLGc({argv[2]},{argv[4]},{argv[6]},{argv[8]})\n', file=f)
-            print(Printer.generate(limit, generations), file=f)
-    def generate(limit, generations):
-        _= []
-        g= 0
-        while g < generations:
-            it= All.be(limit)
+            print(Printer.generate(Limit, Generations), file=f)
+
+    def generate(Limit, Generations):
+        _ = []
+        g = 0
+
+        while g < Generations:
+            it = All.be(Limit)
             _ += [it]
             g += 1
-            print(f'Generation {g}/{generations}')
+            print(f'Generation {g}/{Generations}')
+
         return _
 
 def start():
     if len(argv) != 9:
         print(f'Expected {9} arguments, but received {len(argv)}.')
         return
+
     n = argv[1]
     L = argv[3]
     G = argv[5]
     c = argv[7]  # Superposition
 
     if n != "-n" or L != "-L" or G != "-G" or c != "-c":
-        print(f'Usage: python3 god.py -n a.txt -L 1 -G 1 -c 0')
+        print(f'Usage: python3 nlgc.py -n a.txt -L 1 -G 1 -c 1')
         return
 
     name = argv[2]
