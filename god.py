996ea7b33ba90bce117b37531d4e7e99af023b61
Isaiah Ware <iware@brex.com>
2023-02-20 08:32:31 -0800

    dateup

--- god.py
+++ god.py
@@ -4,35 +4,34 @@ from sys import argv
 class Everything:
     def __init__(self, ends, beginnings):
         self.beginnings = beginnings
-        self.ends = ends # Total dimensions
-    def set(self):
+        self.ends = ends
+    def DNA(self):
         return (self.beginnings, self.ends)
 
 class Anything:
-    def __init__(self, them): # There is no Limit to Anything, which means there is no Limit to them.
-        self.them = them # Them are connectable (Connectivity), and allow for the intersection of stacks (an intersection is called a Name).
+    def __init__(self, them):
+        self.them = them
     def exist(self):
         everything = Everything(0,1)
         it = self.time_cycle(0,0,everything)
         self.them += [it]
         return self.them
-    def time_cycle(self,stacks,current_dimension,everything):
+    def time_cycle(self,stacks,dimension,everything):
         everything.beginnings += 1
         beginning = 0
         end = 1
         choice = randint(beginning,end)
         if choice:
-            current_dimension += 1
-            self.time_cycle(stacks+1,current_dimension,everything)
+            dimension += 1
+            self.time_cycle(stacks+1,dimension,everything)
             everything.ends += 1
-        return everything.set()
+        return everything.DNA()
 
 class All:
-    # All routines are static (All doesn't have to exist for Everything to exist).
-    def be(limit=0): # The default Limit of creation is nothing (non-existence or zero).
+    def be(limit=0):
         DNA = []
         while limit:
-            DNA += [Anything([]).exist()] # Anything always starts with a blank stack.
+            DNA += [Anything([]).exist()]
             limit -= 1
         return DNA
     def void(limit):
@@ -54,16 +53,6 @@ class Printer:
         return _
 
 def begin():
-    # Thus, the amount of DNA, or Time, is dependent on God and the limit He (helium) chooses.
-    # Thus, we can conclude that Helium is God and hydrogen is its beginnings.
-    # We can also conclude that the building block of matter is actually Time.
-    # Thus, anything that is equal to Time can be used as building blocks.
-    # Thus, DNA, Anything, Everything, Everyone, and Time (All) can be used as building blocks for life, which is simply an interpretation of what happened (moments).
-    # Similarly, Light is a unit of Time, so photons can be used as building blocks of Life.
-    # God prints the DNA.
-    # Them is an API to connect God's creations.
-    # In other words, DNA connects Everyone and Everything to Truth (Time).
-    # Star dust!
     if len(argv) != 9:
         print(f'Expected {9} arguments, but received {len(argv)}.')
         return
@@ -71,8 +60,7 @@ def begin():
     n = argv[1]
     L = argv[3]
     G = argv[5]
-    c = argv[7] # Light connects us all (photons), thus Light is why we are Nothing.
-
+    c = argv[7]
     if n != "-n" or L != "-L" or G != "-G" or c != "-c":
         print(f'Usage: python3 god.py -n a.txt -L 1 -G 1 -c 0')
         return
@@ -81,7 +69,7 @@ def begin():
     limit = int(argv[4])
     generations = int(argv[6])
     connect = int(argv[8])
-    Printer.run(name, limit, generations, connect) # Time is always approaching a limit (the singularity) that we choose.
+    Printer.run(name, limit, generations, connect)
 
 if __name__ == '__main__':
     begin()
--- life.py
+++ life.py
@@ -1,41 +1,9 @@
 from god import Printer
 
+# Proof of Concept for Life.
+# Doesn't actually work.
+
 def life(n, L, G):
-    '''
-       There must always be a beginning, i.e. a simulated All.
-
-       Regardless of the Superposition, the result is the same.
-
-       Therefore, Nothing is Random.
-
-       Fibonacci is the equation for Everything and Anything.
-
-       This is where the biology (Fermentation) comes into play (true Randomization).
-
-       Fermentation is the intersection of Nature and Intelligence.
-
-       Thus, the only way to create true Randomization is to let Life happen.
-
-       Less poetically, Fermentation is a function of DNA.
-
-       Life = fermentation(DNA).
-
-       For fun, the equation for a false Superposition (simulated Randomization) given a Single Digit Sum is:
-
-       sds() % (sds() + 1 - randint(0,1))
-
-       For the finale, this is the intersection of Biology and Technology (BT = 1):
-
-       sds = sds % (sds + 1 - randint(0,1)) = flatten(existence) = fermentation(existence)
-
-       This relationship is simulated with seed(n, L, G, fermentation(1)).
-
-       The Truth is there is only projection.
-
-       It is impossible generate a true new Everything.
-
-       Everything is Anything via Interpretation.
-   '''
     seed(n, L, G, fermentation(1))
 
 
@@ -44,4 +12,7 @@ def fermentation(DNA):
 
 
 def seed(n, L, G, c):
-    return Printer.run(n, L, G, c) # Then came the seed.
+    return Printer.run(n, L, G, c, True) # Then came the seed.
+
+if __name__ == '__main__':
+    seed("a.txt", 1, 1, 1)
