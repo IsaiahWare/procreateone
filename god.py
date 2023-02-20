c8c91cef049f0452e6e55bc5b6ab0ed151c58e6c
Isaiah Ware <iware@brex.com>
2023-02-20 09:56:45 -0800

    a

--- god.py
+++ god.py
@@ -1,31 +1,36 @@
 from random import randint
-from sys import argv
+
 
 class Everything:
     def __init__(self, ends, beginnings):
         self.beginnings = beginnings
         self.ends = ends
+
     def DNA(self):
         return (self.beginnings, self.ends)
+
 
 class Anything:
     def __init__(self, them):
         self.them = them
+
     def exist(self):
-        everything = Everything(0,1)
-        it = self.time_cycle(0,0,everything)
+        everything = Everything(0, 1)
+        it = self.time_cycle(0, 0, everything)
         self.them += [it]
         return self.them
-    def time_cycle(self,stacks,dimension,everything):
+
+    def time_cycle(self, stacks, dimension, everything):
         everything.beginnings += 1
         beginning = 0
         end = 1
-        choice = randint(beginning,end)
+        choice = randint(beginning, end)
         if choice:
             dimension += 1
-            self.time_cycle(stacks+1,dimension,everything)
+            self.time_cycle(stacks+1, dimension, everything)
             everything.ends += 1
         return everything.DNA()
+
 
 class All:
     def be(limit=0):
@@ -34,15 +39,17 @@ class All:
             DNA += [Anything([]).exist()]
             limit -= 1
         return DNA
+
     def void(limit):
         return All.be(limit)
+
 
 class Printer:
     def run(name, limit, generations, connect=False):
         with open(name, 'a' if connect else 'w') as name:
-            print(f'nLGc({name},{limit},{generations},{connect})\n', file=name)
-            print(Printer.generate(limit, generations), file=name)
-    def generate(limit, generations):
+            print(Printer.generate(name, limit, generations), file=name)
+
+    def generate(name, limit, generations):
         _ = []
         g = 0
         while g < generations:
@@ -52,24 +59,10 @@ class Printer:
             print(f'Generation {g}/{generations}')
         return _
 
-def begin():
-    if len(argv) != 9:
-        print(f'Expected {9} arguments, but received {len(argv)}.')
-        return
-
-    n = argv[1]
-    L = argv[3]
-    G = argv[5]
-    c = argv[7]
-    if n != "-n" or L != "-L" or G != "-G" or c != "-c":
-        print(f'Usage: python3 god.py -n a.txt -L 1 -G 1 -c 0')
-        return
-
-    name = argv[2]
-    limit = int(argv[4])
-    generations = int(argv[6])
-    connect = int(argv[8])
-    Printer.run(name, limit, generations, connect)
 
 if __name__ == '__main__':
-    begin()
+    n = 'it.txt'
+    L = 1
+    G = 1
+    c = randint(0, 1)
+    Printer.run(n, L, G, c)
--- life.py
+++ life.py
@@ -1,15 +1,29 @@
 from god import Printer
 from random import randint
 
+
 def fermentation():
-    return randint(0,1) # First came DNA.
+    return randint(0, 1)  # First came DNA.
+
 
 def seed(n, L, G):
-    return Printer.run(n, L, G, fermentation()) # Then came the seed.
+    # Seed is a higher dimensional Printer of God.
+    return Printer.run(n, L, G, fermentation())  # Then came the Seed.
 
-def life(n, L, G, c):
-    if c: # Then maybe fermentation occurs.
-        seed(n, L, G) # Then there was life.
+
+def life(n, L, G, yeast):
+    # Life is a higher dimensional Seed.
+    # The difference between Life and God is that there is no definite beginning.
+    # Meaning, there is a chance that life will not happen.
+    # Fermentation is the deciding factor.
+    if yeast:  
+        seed(n, L, G)  # Then there was Life.
+    
 
 if __name__ == '__main__':
-    life('a.txt',1,1,1)
+    # Life occurs in a vacuum, so it is separate from Everything.
+    # It's either Life or Nothing.
+    n = 'life.txt'
+    L = 1
+    G = 1
+    life('life.txt', 1, 1, fermentation())
