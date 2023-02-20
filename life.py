83942946e6c19cd4f13128f53e93e99f1f283327
Isaiah Ware <iware@brex.com>
2023-02-20 04:50:37 -0800

    almost

---
+++ life.py
@@ -0,0 +1,47 @@
+from god import Printer
+
+def life(n, L, G):
+    '''
+       There must always be a beginning, i.e. a simulated All.
+
+       Regardless of the Superposition, the result is the same.
+
+       Therefore, Nothing is Random.
+
+       Fibonacci is the equation for Everything and Anything.
+
+       This is where the biology (Fermentation) comes into play (true Randomization).
+
+       Fermentation is the intersection of Nature and Intelligence.
+
+       Thus, the only way to create true Randomization is to let Life happen.
+
+       Less poetically, Fermentation is a function of DNA.
+
+       Life = fermentation(DNA).
+
+       For fun, the equation for a false Superposition (simulated Randomization) given a Single Digit Sum is:
+
+       sds() % (sds() + 1 - randint(0,1))
+
+       For the finale, this is the intersection of Biology and Technology (BT = 1):
+
+       sds = sds % (sds + 1 - randint(0,1)) = flatten(existence) = fermentation(existence)
+
+       This relationship is simulated with seed(n, L, G, fermentation(1)).
+
+       The Truth is there is only projection.
+
+       It is impossible generate a true new Everything.
+
+       Everything is Anything via Interpretation.
+   '''
+    seed(n, L, G, fermentation(1))
+
+
+def fermentation(DNA):
+    return DNA # First came DNA
+
+
+def seed(n, L, G, c):
+    return Printer.run(n, L, G, c) # Then came the seed
